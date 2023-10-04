a094b10c03ffa46729b950deceb49f4b72c51196
dansim6935 <dansim6935@gmail.com>
2023-10-04 10:37:31 -0700

    Alert user if both password and username was not provided added to login.js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,13 +1,22 @@
 const loginFormHandler = async (event) => {
-  // Stop the browser from submitting the form so we can do so with JavaScript
   event.preventDefault();
 
-  // Gather the data from the form elements on the page
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
+  if (!email && !password) {
+
+    alert("Please provide your username and password.");
+    return;
+  } else if (!email) {
+    alert("Please provide your username and password.");
+    return;
+  } else if (!password) {
+    alert("Please provide your username and password.");
+    return;
+  }
+
   if (email && password) {
-    // Send the e-mail and password to the server
     const response = await fetch("/api/user", {
       method: "POST",
       body: JSON.stringify({email, password}),
@@ -25,5 +34,3 @@ const loginFormHandler = async (event) => {
 document
   .querySelector(".loginForm")
   .addEventListener("submit", loginFormHandler);
-
-  
