88710171905733c183019a4daf90592fd685822f
Cossack <kkowalak9@gmail.com>
2023-10-05 11:46:38 -0700

    Semantic changes to few files

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,28 +1,26 @@
 const loginFormHandler = async (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+    const email = document.querySelector("#email-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
+    
+    if (!email || !password) {
+        alert("Please provide your username and password.");
+        return;
+    };
 
-  if (!email || !password) {
-
-    alert("Please provide your username and password.");
-    return;
-  };
-
-  if (email && password) {
-    const response = await fetch("/api/user", {
-      method: "POST",
-      body: JSON.stringify({email, password}),
-      headers: {"Content-Type": "application/json"},
-    });
-
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert("Failed to log in");
+    if (email && password) {
+        const response = await fetch("/api/user", {
+            method: "POST",
+            body: JSON.stringify({email, password}),
+            headers: {"Content-Type": "application/json"},
+        });
+        if (response.ok) {
+            document.location.replace("/");
+        } else {
+            alert("Failed to log in");
+        }
     }
-  }
 };
 
 document
