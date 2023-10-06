35d22882eed73abb77a542d5fffc9bea64c2f0aa
dansim6935 <dansim6935@gmail.com>
2023-10-06 11:27:14 -0700

    Finished instance method in Ticket.js

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,8 +1,29 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 const Log = require("./Log"); 
+const { findDiff, formatDate } = require("..utils/helpers");
 
-class Ticket extends Model {}
+class Ticket extends Model {
+  logChange(userId, oldData) {
+    // This method will use the findDiff helper method, passing along the changed ticket instance and the previous ticket values.
+    const changes = findDiff(this, oldData);
+
+    // This method will return early if no changes are found.
+    if (changes.length === 0) {
+      return;
+    };
+
+    const date = new Date()
+
+    // Create a Log entry
+    return Log.create({
+      type: "Modified",
+      message: `${changes.length} changes were made on ${formatDate(date)} by ${userId}.`,
+      userId: userId, // The Log.userId field's value will be passed as the argument to this method
+      ticketId: this.id, // The Log.ticketId field will be a reference to the instance id
+    });
+  }
+};
 
 Ticket.init(
   {
